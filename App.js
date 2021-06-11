cb2a0e2893a02108e859bc3f9e88866e77777ebc
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-06-11 00:15:22 -0300

    funcion de obtener un array con valores de la matriz ingresada

--- App.js
+++ App.js
@@ -14,7 +14,12 @@ document.getElementById("btngenerateValuesInputInterface")
     event.preventDefault();
     generateValuesInputInterface(divMatrixA, sizeM_A.value, sizeN_A.value);
     generateValuesInputInterface(divMatrixB, sizeM_B.value, sizeN_B.value);
-}, { once: true })
+    // save sizes:
+    sessionStorage.setItem("size_m_A", sizeM_A.value);
+    sessionStorage.setItem("size_n_A", sizeN_A.value);
+    sessionStorage.setItem("size_m_B", sizeM_B.value);
+    sessionStorage.setItem("size_n_B", sizeN_B.value);
+}, { once: true });
 
 function generateValuesInputInterface(divMatrix, mSize, nSize) {
     const M = parseInt(mSize);   // rows
@@ -30,32 +35,42 @@ function generateValuesInputInterface(divMatrix, mSize, nSize) {
             input.value = 0;
             input.type = "number"
             input.classList.add("valuesMatrixInput");
-
             column.appendChild(input)
             row.appendChild(column);
         }
-
-        
         table.appendChild(row);
     }
+    
     divMatrix.appendChild(table)
+}
 
-    // <table>
-    //             <tr>  <!-- fila -->
-    //                 <th>xd</th>  <!-- columna -->
-    //                 <th>xd</th>
-    //                 <th>xd</th>
-    //                 <th>xd</th>
-    //             </tr>
-    //             <tr>
-    //                 <th>xd</th>
-    //                 <th>xd</th>
-    //                 <th>xd</th>
-    //                 <th>xd</th>
-    //             </tr>
-    //         </table>
+document.getElementById("btnCalculate").addEventListener("click", (event) => {
+    event.preventDefault()
+    calculate()
+}, { once: true })
 
+function calculate() {
+    getMatrixValues(divMatrixA)
+}
 
+/**
+ * @param matrix as [object HTMLDivElement]
+ */
+function getMatrixValues(matrixElem) {
+    const matrix = matrixElem.children[0];
+    let matrixValues = [];
 
+    for (let index = 0; index < matrix.childElementCount; index++)
+    {
+        let row = [];
+        for (let j = 0; j < matrix.children[0].childElementCount; j++) {
+            row.push(
+                parseInt(matrix.children[index].children[j].children[0].value)
+            );
+        }
+        matrixValues.push(row);
+    }
+
+    return matrixValues;
 }
 
